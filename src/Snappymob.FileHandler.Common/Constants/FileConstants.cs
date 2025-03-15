6b39e7e2f58cb68e2b12eea57228c2365b3dd508
mdmithuncse <mdmithuncse@gmail.com>
2025-03-15 18:56:43 +0600

    feat SFH-001: Implement project structure

---
+++ src/Snappymob.FileHandler.Common/Constants/FileConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snappymob.FileHandler.Common.Constants
+{
+    internal class FileConstants
+    {
+    }
+}
