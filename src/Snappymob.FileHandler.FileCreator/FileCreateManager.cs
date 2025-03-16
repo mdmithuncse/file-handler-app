7a072ebe6ef6a0438c6b019600fa1362170a6e03
mdmithuncse <mdmithuncse@gmail.com>
2025-03-16 12:07:18 +0600

    feat SFH-004: Implemented random object service and dependency injection into console application

---
+++ src/Snappymob.FileHandler.FileCreator/FileCreateManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Snappymob.FileHandler.Service.RandomObject;
+
+namespace Snappymob.FileHandler.FileCreator
+{
+    internal class FileCreateManager : IHostedService
+    {
+        private readonly IRandomObjectService _randomObjectService;
+
+        public FileCreateManager(IRandomObjectService randomObjectService)
+        {
+            _randomObjectService = randomObjectService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Hello, This is file creator app! File creator app is starting.");
+
+            string randomObject = _randomObjectService.GenerateRandomObject();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("File creator app is stopping.");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/Snappymob.FileHandler.FileCreator/Program.cs
+++ src/Snappymob.FileHandler.FileCreator/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, This is file creator app!");
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Snappymob.FileHandler.Service.RandomObject;
+
+namespace Snappymob.FileHandler.FileCreator
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<IRandomObjectService, RandomObjectService>();
+                    services.AddHostedService<FileCreateManager>();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
